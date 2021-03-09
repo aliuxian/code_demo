3c8f51471bd764eb11fc19e8626a2000ca1f6dcd
liuxian <liuxian@xiaomi.com>
2021-03-09 16:48:45 +0800

    Flink Broadcast And Accumulator Demo

--- bigdataExample/src/main/java/flink/batch/AccumulatorDemo.java
+++ bigdataExample/src/main/java/flink/batch/AccumulatorDemo.java
@@ -1,8 +1,53 @@
 package flink.batch;
 
+import org.apache.flink.api.common.JobExecutionResult;
+import org.apache.flink.api.common.accumulators.IntCounter;
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.operators.MapOperator;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.core.fs.FileSystem;
+
 public class AccumulatorDemo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        DataSource<String> data = env.fromElements("a", "b", "c", "d", "e", "f", "g", "h");
+
+        MapOperator<String, String> result = data.map(new RichMapFunction<String, String>() {
+
+
+            private IntCounter numLines = new IntCounter();
+
+            @Override
+            public void open(Configuration parameters) throws Exception {
+                super.open(parameters);
+                getRuntimeContext().addAccumulator("num-lines", this.numLines);
+            }
+
+            @Override
+            public String map(String s) throws Exception {
+                this.numLines.add(1);
+                // 如果在这里获取累加器的值
+                // 并行度为1的情况下，该累加器的结果才正确，多并行度下结果会出错。
+                System.out.println(numLines.getLocalValue() + "***");
+                return s;
+            }
+        }).setParallelism(3);
+
+//        result.print();
+
+
+        result.writeAsText("/tmp/output", FileSystem.WriteMode.OVERWRITE);
+
+        // 累加器的结果存在JobExecutionResult中，而JobExecutionResult是execute方法返回的，所以必须要等待作业执行完成才可以
+        JobExecutionResult jobResult = env.execute("ACC");
+        int accumulatorResult = jobResult.getAccumulatorResult("num-lines");
+
+        System.out.println(accumulatorResult);
+
 
     }
 }
--- bigdataExample/src/main/java/flink/batch/BroadcastDemo.java
+++ bigdataExample/src/main/java/flink/batch/BroadcastDemo.java
@@ -1,8 +1,74 @@
 package flink.batch;
 
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class BroadcastDemo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        // 程序入口
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+
+        // 需要广播的数据
+        ArrayList<Tuple2<String, Integer>> broadData = new ArrayList<>();
+        broadData.add(new Tuple2<>("zs",18));
+        broadData.add(new Tuple2<>("ls",20));
+        broadData.add(new Tuple2<>("ww",17));
+        DataSet<Tuple2<String, Integer>> tupleData = env.fromCollection(broadData);
+
+        //1.1:处理需要广播的数据,把数据集转换成map类型，map中的key就是用户姓名，value就是用户年龄
+        DataSet<HashMap<String, Integer>> toBroadcast = tupleData.map(new MapFunction<Tuple2<String, Integer>, HashMap<String, Integer>>() {
+            @Override
+            public HashMap<String, Integer> map(Tuple2<String, Integer> value) throws Exception {
+                HashMap<String, Integer> res = new HashMap<>();
+                res.put(value.f0, value.f1);
+                return res;
+            }
+        });
+
+        //源数据
+        DataSource<String> data = env.fromElements("zs", "ls", "ww", "zl");
+
+
+        DataSet<String> result = data.map(new RichMapFunction<String, String>() {
+            List<HashMap<String, Integer>> broadCastMap = new ArrayList<HashMap<String, Integer>>();
+
+            HashMap<String, Integer> allMap = new HashMap<String, Integer>();
+
+            /**
+             * 这个方法只会执行一次
+             * 可以在这里实现一些初始化的功能
+             * 所以，就可以在open方法中获取广播变量数据
+             */
+            @Override
+            public void open(Configuration parameters) throws Exception {
+                super.open(parameters);
+                //3:获取广播数据
+                this.broadCastMap = getRuntimeContext().getBroadcastVariable("broadCastMapName");
+                for (HashMap map : broadCastMap) {
+                    allMap.putAll(map);
+                }
+            }
+            @Override
+            public String map(String key) throws Exception {
+                Integer age = allMap.getOrDefault(key, 0);
+                return key + "," + age;
+            }
+        }).withBroadcastSet(toBroadcast, "broadCastMapName");//2：执行广播数据的操作
+
+
+        result.print();
+
 
     }
 }
